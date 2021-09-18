bc9104abb8bd56275b4b3eee7f6651071ebd373a
Janson Harold Siy <jansonharold.siy@gmail.com>
2021-09-18 23:33:29 +0800

    followed instructions in creating mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'fortestingprojects123@gmail.com'
   layout 'mailer'
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,22 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # Instructions from https://medium.com/@ericschwartz7/rails-mailer-tutorial-82700f6737d9
+  config.action_mailer.delivery_method = :sendmail
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   location: '/usr/sbin/sendmail',
+  #   arguments: '-i'
+  # }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'smtp.gmail.com',
+    user_name:            'fortestingprojects123@gmail.com',
+    password:             'testingprojects',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+
 end
